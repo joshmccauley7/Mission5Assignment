92c4d7eb48e05125a08eff0d7b669edf292bee7a
joshmccauley7 <joshmccauley7@gmail.com>
2026-02-02 23:20:43 -0700

    calculator page

--- Mission5Assignment/wwwroot/js/site.js
+++ Mission5Assignment/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$("#btnSubmit").click(function () {
+    if ($("#hours").val() < 0)
+    {
+        $("#output").val("Enter a postive number")
+    }
+    if($("#hours").val() >= 0)
+    {
+        $("#output").val("Charge is: $" + $("#hours").val() * $("#charge").val())
+    }
+})
